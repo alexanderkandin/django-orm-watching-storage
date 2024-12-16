8e42c94f96b39908aaa77e1cf318916d840e5062
Alex <1kimalexander@gmail.com>
2024-12-16 23:11:05 +0400

    Изменил try-except на get_404

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,17 +1,18 @@
-from datacenter.models import Passcard, is_visit_long
-from datacenter.models import Visit, get_duration, format_duration
-from django.shortcuts import render
+
+
+from datacenter.models import Passcard
+from datacenter.models import Visit
+from django.shortcuts import render, get_object_or_404
+from datacenter.helper_func import get_duration, format_duration, is_visit_long
 import locale
 
 
 def passcard_info_view(request, passcode):
     locale.setlocale(locale.LC_TIME, 'russian')
 
-    try:
-        passcard = Passcard.objects.get(passcode=passcode)
-    except Passcard.DoesNotExist:
-        print("Нет такого ID")
-        passcard = None
+
+    passcard = get_object_or_404(Passcard,passcode=passcode)
+
 
     if passcard:
         visits = Visit.objects.filter(passcard=passcard)
