bd0697e9155e28aeb85499ff7485171220d83a60
Alex <1kimalexander@gmail.com>
2024-12-16 23:07:42 +0400

    Удалил лишние функции

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now, utc
 
 class Passcard(models.Model):
     is_active = models.BooleanField(default=False)
@@ -30,18 +29,3 @@ class Visit(models.Model):
         )
 
 
-def get_duration(visitor):
-    entered_at = visitor.entered_at
-    leaved_at = visitor.leaved_at or now().astimezone(utc)
-    duration = leaved_at - entered_at
-    return duration
-
-def format_duration(duration):
-    total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    return f"{hours}ч {minutes}мин"
-
-def is_visit_long(visit, minutes=60):
-    is_strange = visit > minutes*60
-    return is_strange
