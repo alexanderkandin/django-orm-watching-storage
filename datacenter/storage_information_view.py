27ae6a75a77237ef46780e993cadb6865ccb6e41
Alex <1kimalexander@gmail.com>
2024-12-18 08:39:06 +0400

    Применил .filter

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,17 +4,16 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    all_visitors = Visit.objects.all()
+    storage_visitors = Visit.objects.filter(leaved_at__isnull=True)
     non_closed_visits = []
 
-    for visitor in all_visitors:
-        if visitor.leaved_at is None:
-            duration = get_duration(visitor)
-            format_time = format_duration(duration)
-            visitor_info = {
-            'who_entered': visitor.passcard,'entered_at': visitor.entered_at.strftime('%Y-%m-%d %H:%M:%S'), 'duration': format_time
-            }
-            non_closed_visits.append(visitor_info)
+    for visitor in storage_visitors:
+        duration = get_duration(visitor)
+        format_time = format_duration(duration)
+        visitor_info = {
+        'who_entered': visitor.passcard,'entered_at': visitor.entered_at.strftime('%Y-%m-%d %H:%M:%S'), 'duration': format_time
+        }
+        non_closed_visits.append(visitor_info)
 
     context = {
         'non_closed_visits': non_closed_visits,
